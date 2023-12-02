26ed9bbc513c8b1edb201f8a2ef56278e01166ce
Jonathan Chang <jonathanchang@netflix.com>
2023-12-02 15:53:25 -0800

    style

--- advent-of-code-2023/tailwind.config.js
+++ advent-of-code-2023/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // that is animation class
+      animation: {
+        fade: "fadeOut 2s ease-in-out",
+      },
+
+      // that is actual animation
+      keyframes: (theme) => ({
+        fadeOut: {
+          "0%": { color: theme("colors.green.600") },
+          "100%": { color: theme("colors.red.900") },
+        },
+      }),
+    },
   },
   plugins: [],
 };
