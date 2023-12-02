c93669d12fd68a68cc35d74f41c7d199f93f58a4
Jonathan Chang <jonathanchang@netflix.com>
2023-12-01 23:57:04 -0800

    day 2 part 2

--- day2/index.ts
+++ day2/index.ts
@@ -4,32 +4,37 @@ import { getData } from "../utils/readFile";
 const run = async (file: string) => {
   const input = await getData(path.resolve(__dirname, file));
 
-  let answer = (input.length * (input.length + 1)) / 2;
+  // part 1
+  // let answer = (input.length * (input.length + 1)) / 2;
+
+  // part 2
+  let answer = 0;
 
   input.forEach((game) => {
     const gameData = game.split(":");
     const draws = gameData[1].split(";");
     console.log(gameData[0]);
+    let redResult = 0,
+      blueResult = 0,
+      greenResult = 0;
     for (const draw of draws) {
-      let redResult = 0,
-        blueResult = 0,
-        greenResult = 0;
       draw.split(",").map((color) => {
         const colorDraw = color.trim();
         if (colorDraw.indexOf("red") > -1) {
-          redResult = parseInt(colorDraw, 10);
+          redResult = Math.max(parseInt(colorDraw, 10), redResult);
         } else if (colorDraw.indexOf("blue") > -1) {
-          blueResult = parseInt(colorDraw, 10);
+          blueResult = Math.max(parseInt(colorDraw, 10), blueResult);
         } else if (colorDraw.indexOf("green") > -1) {
-          greenResult = parseInt(colorDraw, 10);
+          greenResult = Math.max(parseInt(colorDraw, 10), greenResult);
         }
       });
-      if (redResult > 12 || greenResult > 13 || blueResult > 14) {
-        answer = answer - parseInt(gameData[0].split(" ")[1], 10);
-        console.log("game failed");
-        break;
-      }
+      // if (redResult > 12 || greenResult > 13 || blueResult > 14) {
+      //   answer = answer - parseInt(gameData[0].split(" ")[1], 10);
+      //   console.log("game failed");
+      //   break;
+      // }
     }
+    answer += redResult * greenResult * blueResult;
   });
   console.log(answer);
 };
