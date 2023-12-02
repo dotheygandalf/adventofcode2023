76832e0044322baec6b55dc289775df0760586b4
Jonathan Chang <jonathanchang@netflix.com>
2023-12-01 22:14:37 -0800

    util

--- utils/readFile.ts
+++ utils/readFile.ts
@@ -1,12 +1,13 @@
 import * as fs from "fs";
-import * as path from "path";
 
-export const getData = () => {
-  fs.readFile(path.resolve(__dirname, "./input.txt"), "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    return data;
+// path.resolve(__dirname, "./input.txt")
+export const getData = async (path: string) => {
+  return new Promise<string[]>((res, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      res(data.split("\n"));
+    });
   });
 };
