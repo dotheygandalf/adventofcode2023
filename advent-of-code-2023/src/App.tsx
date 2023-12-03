9d2fc1875acf1b882ddfc7a4a2f3b609166e7801
Jonathan Chang <jonathanchang@netflix.com>
2023-12-02 17:16:21 -0800

    hook up router

--- advent-of-code-2023/src/App.tsx
+++ advent-of-code-2023/src/App.tsx
@@ -1,20 +1,12 @@
+import { Link } from "react-router-dom";
 import "./App.css";
-import { Day } from "./Day";
-import { Day1 } from "./advent/day1/Day1";
 
 const App = () => {
   return (
     <>
-      <Day
-        number={1}
-        prompt="Find the first and last number. Then combine them to make a two
-  digit number."
-      >
-        <Day1 />
-      </Day>
-      <footer className="text-right pt-10 pb-10 pr-10">
-        © <a href="https://hellojonathanchang.com">Jonathan Chang</a> 2023
-      </footer>
+      <h1>
+        <Link to={"day1"}>Day 1</Link>
+      </h1>
     </>
   );
 };
--- advent-of-code-2023/src/main.tsx
+++ advent-of-code-2023/src/main.tsx
@@ -3,11 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Day } from "./Day.tsx";
+import { Day1 } from "./advent/day1/Day1.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/adventofcode2023",
     element: <App />,
+  },
+  {
+    path: "/adventofcode2023/day1",
+    element: (
+      <>
+        <Day
+          number={1}
+          prompt="Find the first and last number. Then combine them to make a two
+digit number."
+        >
+          <Day1 />
+        </Day>
+        <footer className="text-right pt-10 pb-10 pr-10">
+          © <a href="https://hellojonathanchang.com">Jonathan Chang</a> 2023
+        </footer>
+      </>
+    ),
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
