56bdb37b4249334e99f6584def65496a794d5daf
Jonathan Chang <jonathanchang@netflix.com>
2023-12-02 21:04:18 -0800

    use has router

--- advent-of-code-2023/src/main.tsx
+++ advent-of-code-2023/src/main.tsx
@@ -2,51 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Day } from "./Day.tsx";
 import { Day1 } from "./advent/day1/Day1.tsx";
 import { Day2 } from "./advent/Day2.tsx";
 
 console.log(import.meta.env.MODE);
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/adventofcode2023",
-      element: <App />,
-    },
-    {
-      path: "/adventofcode2023/day1",
-      element: (
-        <>
-          <Day
-            number={1}
-            prompt="Find the first and last number. Then combine them to make a two
-digit number."
-          >
-            <Day1 />
-          </Day>
-        </>
-      ),
-    },
-    {
-      path: "/adventofcode2023/day2",
-      element: (
-        <>
-          <Day
-            number={2}
-            prompt="Find the min number of each colored cubes needed for the draws to be possible."
-          >
-            <Day2 />
-          </Day>
-        </>
-      ),
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: import.meta.env.MODE !== "development" ? "/adventofcode2023" : "",
-  }
-);
+    path: "/adventofcode2023",
+    element: <App />,
+  },
+  {
+    path: "/adventofcode2023/day1",
+    element: (
+      <>
+        <Day
+          number={1}
+          prompt="Find the first and last number. Then combine them to make a two
+digit number."
+        >
+          <Day1 />
+        </Day>
+      </>
+    ),
+  },
+  {
+    path: "/adventofcode2023/day2",
+    element: (
+      <>
+        <Day
+          number={2}
+          prompt="Find the min number of each colored cubes needed for the draws to be possible."
+        >
+          <Day2 />
+        </Day>
+      </>
+    ),
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
