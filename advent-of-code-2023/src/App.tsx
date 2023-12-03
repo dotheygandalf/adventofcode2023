21955296118915b2a5eeb0f9b96dd590df41b47c
Jonathan Chang <jonathanchang@netflix.com>
2023-12-02 16:55:29 -0800

    footer

--- advent-of-code-2023/src/App.tsx
+++ advent-of-code-2023/src/App.tsx
@@ -4,13 +4,18 @@ import { Day1 } from "./advent/day1/Day1";
 
 const App = () => {
   return (
-    <Day
-      number={1}
-      prompt="Find the first and last number. Then combine them to make a two
+    <>
+      <Day
+        number={1}
+        prompt="Find the first and last number. Then combine them to make a two
   digit number."
-    >
-      <Day1 />
-    </Day>
+      >
+        <Day1 />
+      </Day>
+      <footer className="text-right pt-10 pb-10 pr-10">
+        © <a href="https://hellojonathanchang.com">Jonathan Chang</a> 2023
+      </footer>
+    </>
   );
 };
 
